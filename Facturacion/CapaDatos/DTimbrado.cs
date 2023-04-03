38305b0ceb6e01e3f17ae5197147844f6efca5b7
MarcoGV182 <markgonzalez1@hotmail.com>
2023-04-03 09:25:24 -0400

    Se crea la clase DTimbrado

---
+++ Facturacion/CapaDatos/DTimbrado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DTimbrado
+    {
+        public int IdTimbrado { get; set; }
+        public string NroTimbrado { get; set; }
+        public string FechaInicioVigencia { get; set; }
+        public string FechaFinVigencia { get; set; }
+        public string Estado { get; set; }
+    }
+}
